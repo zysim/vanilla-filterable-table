6bd2361d1fa453f04550f154df84fe1c3a442bd7
Sim <me@zhongyuen.dev>
2021-06-21 22:43:08 +0800

    We Can Filter Stuff

--- src/ThFilterable.ts
+++ src/ThFilterable.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import Fuse from 'https://cdn.jsdelivr.net/npm/fuse.js@6.4.1/dist/fuse.basic.esm.js'
+
 export type FilterCallback = (type?: string) => void
 export type ResetCallback = () => void
 
@@ -26,7 +29,7 @@ class ThFilterable extends HTMLElement {
           justify-content: center;
         }
 
-        .filterContainer {
+        .buttonAndFilterListContainer {
           display: flex;
           flex-direction: column;
           position: absolute;
@@ -43,23 +46,31 @@ class ThFilterable extends HTMLElement {
           cursor: pointer;
         }
 
-        .filterList {
+        .filterListContainer {
           border-collapse: collapse;
           visibility: collapse;
           background: white;
-          list-style: none;
-          text-align: left;
           border: solid 1px black;
-          padding: 0;
-          margin: 0;
           max-height: 50vh;
           max-width: 10vw;
           overflow-x: hidden;
           overflow-y: scroll;
         }
 
-        .filterList.visible {
+        .filterListContainer.visible {
           visibility: visible;
+        }
+
+        .search {
+          padding: 1rem;
+          font-size: 1rem;
+        }
+
+        .filterList {
+          list-style: none;
+          text-align: left;
+          padding: 0;
+          margin: 0;
         }
 
         .filterList:hover {
@@ -80,75 +91,123 @@ class ThFilterable extends HTMLElement {
         }
       </style>
       ${title}
-      <div class="filterContainer">
+      <div class="buttonAndFilterListContainer">
         <button class="button" onclick="">&#x25bc;</button>
-        <ul class="filterList">
-          ${this.getListItems(data)}
-        </ul>
+        <div class="filterListContainer">
+          <input type="text" class="search" placeholder="Search..."/>
+          <ul class="filterList">
+            ${this.createListItemsHtml(data)}
+          </ul>
+        </div>
       </div>
     `
 
     shadow.appendChild(container)
   }
 
+  get filterListContainerEl() {
+    return (this.shadowRoot as ShadowRoot).querySelector(
+      '.filterListContainer',
+    ) as HTMLDivElement
+  }
+
+  get listItemEls() {
+    return (this.shadowRoot as ShadowRoot).querySelectorAll('li')
+  }
+
+  get listItemElsWithoutClear() {
+    return (this.shadowRoot as ShadowRoot).querySelectorAll(
+      'li[data-clear="false"]',
+    ) as NodeListOf<HTMLLIElement>
+  }
+
+  get toggleButtonEl() {
+    return (this.shadowRoot as ShadowRoot).querySelector(
+      '.button',
+    ) as HTMLButtonElement
+  }
+
+  get searchInputEl() {
+    return (this.shadowRoot as ShadowRoot).querySelector(
+      '.search',
+    ) as HTMLInputElement
+  }
+
   connectedCallback() {
     // Add click listeners
     window.addEventListener('click', this.hideDropdown)
-    this.getShadowRoot().addEventListener('click', this._hideDropdown)
-    this.getShadowRoot()
-      .querySelector('.button')
-      ?.addEventListener('click', this.toggleDropdown)
-    this.getShadowRoot()
-      .querySelectorAll('li')
-      .forEach((li, i) => {
-        li.addEventListener(
-          'click',
-          !i ? this.resetCallback : this.triggerFilterCallback(li),
-        )
-      })
+    this.shadowRoot?.addEventListener('click', this._hideDropdown)
+    this.toggleButtonEl.addEventListener('click', this.toggleDropdown)
+    this.listItemEls.forEach((li, i) => {
+      li.addEventListener(
+        'click',
+        !i ? this.resetCallback : this.triggerFilterCallback(li),
+      )
+      li.addEventListener('click', () => this._hideDropdown(true))
+    })
+
+    // Add search listener
+    this.searchInputEl.addEventListener('input', this.searchListItems)
   }
 
   disconnectedCallback() {
     // Remove click listeners
     window.removeEventListener('click', this.hideDropdown)
-    this.getShadowRoot().removeEventListener('click', this._hideDropdown)
-    this.getShadowRoot()
-      .querySelector('.button')
-      ?.removeEventListener('click', this.toggleDropdown)
-    this.getShadowRoot()
-      .querySelectorAll('li')
-      .forEach(li =>
-        li.removeEventListener('click', this.triggerFilterCallback(li)),
+    this.shadowRoot?.removeEventListener('click', this._hideDropdown)
+    this.toggleButtonEl.removeEventListener('click', this.toggleDropdown)
+    this.listItemEls.forEach((li, i) => {
+      li.removeEventListener(
+        'click',
+        !i ? this.resetCallback : this.triggerFilterCallback(li),
       )
+      li.removeEventListener('click', () => this._hideDropdown(true))
+    })
+
+    // Remove search listener
+    this.searchInputEl.addEventListener('change', this.searchListItems)
   }
+
+  createListItemsHtml = (data: string[]) =>
+    Array.from(
+      new Set(
+        ['Clear']
+          .concat(data)
+          .map((d, i) => `<li data-type="${d}" data-clear="${!i}">${d}</li>`),
+      ),
+    ).join('')
 
   hideDropdown = (e: MouseEvent) => this._hideDropdown(e.target !== this)
 
   _hideDropdown = (e: Event | boolean) => {
     if (typeof e === 'boolean') {
-      e &&
-        this.getShadowRoot()
-          .querySelector('.filterList')
-          ?.classList.remove('visible')
+      e && this.filterListContainerEl.classList.remove('visible')
     } else if (
       !(e.target as Element).closest('.button') &&
-      !(e.target as Element).closest('.filterList')
+      !(e.target as Element).closest('.filterListContainer')
     ) {
-      this.getShadowRoot()
-        .querySelector('.filterList')
-        ?.classList.remove('visible')
+      this.filterListContainerEl.classList.remove('visible')
     }
   }
 
-  getListItems = (data: string[]) =>
-    Array.from(
-      new Set(
-        ['Clear'].concat(data).map(d => `<li data-type="${d}">${d}</li>`),
-      ),
-    ).join('')
-
-  getShadowRoot(): ShadowRoot {
-    return this.shadowRoot as ShadowRoot
+  searchListItems = (ev: Event) => {
+    const inputValue = (ev.target as HTMLInputElement).value
+    if (!inputValue.length) {
+      this.listItemElsWithoutClear.forEach(li => {
+        li.style.display = 'list-item'
+      })
+      return
+    }
+    const fuse = new Fuse(this.listItemElsWithoutClear, {
+      keys: ['dataset.type'],
+    })
+    const result = fuse.search(inputValue) as {
+      item: HTMLLIElement
+    }[]
+    this.listItemElsWithoutClear.forEach(li => {
+      if (!result.find(r => r.item.dataset.type === li.dataset.type)) {
+        li.style.display = 'none'
+      }
+    })
   }
 
   toggleDropdown(this: Element, _: Event) {
